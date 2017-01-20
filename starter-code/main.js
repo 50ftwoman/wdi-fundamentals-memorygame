3cae32a51b93891848132fc8040a561dc39d8a68
Svet Maillet <50ftowmen@gmail.com>
2017-01-19 21:54:04 -0800

    second change commit

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,44 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+/*
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else if (cardThree === cardFour) {
+	alert('You found a match!');
+}
+else {
+	alert('Try again!')
+}
+*/
+
+
+/* var createCards = function () {
+
+}
+*/
+
+
+	var gameBoard = document.getElementById('game-board');
+	var cards = 4;
+function createCards () {
+	for (var i = 0; i < cards; i++) {
+		var cardsDiv = document.createElement('div');
+		cardsDiv.className = 'card';
+		gameBoard.appendChild(cardsDiv)
+	}
+}
+
+whichElem = document.getElementById('cards');
+
+createCards();
+
+
+
+
+
+
+
+
+
