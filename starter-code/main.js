61949b08bd2ca21fc4cc684f5c4bb50f4ea2f686
Svet Maillet <50ftowmen@gmail.com>
2017-01-20 12:27:27 -0800

    starter-code/main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -14,12 +14,15 @@ var createBoard = function() {
     for ( var i = 0; i < cards.length; i++) {
         cardElement.setAttribute('data-card', cards[i]);
         	if ('data-card' === 'king') {
-        		cardElement.innerHTML = '<img src="king.jpg" alt ="King of Hearts" />';
+        		cardElement.innerHTML = '<img src="king.png" alt ="King of Hearts" />';
         	}
         	else if ('data-card' === 'queen') {
-        		cardElement.innerHTML = '<img src="queen.jpg" alt="Queen of Spades" />';
+        		cardElement.innerHTML = '<img src="queen.png" alt="Queen of Spades" />';
         	}
         newCard.addEventListener('click',  isTwoCards);
+        	newCard = getElementById('data-card');
+        		newcard.innerHTML = <'div onclick = '/>
+
         gameBoard.appendChild(newCard);
     }
 }
